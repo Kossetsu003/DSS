103d18e98544acfa32748b0e84a534fbb784d853
mario-90 <105886653+mario-90@users.noreply.github.com>
2025-05-18 00:51:03 -0600

    a

--- backend/index.js
+++ backend/index.js
@@ -12,36 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
-
-app.post('/registro', async (req, res) => {
-  const { carnet, password } = req.body;
-  console.log(req.body);
-
-  // Validación de contraseña
-  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}$/;
-
-  if (!regex.test(password)) {
-    return res.status(400).json({
-      mensaje: 'La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula y un símbolo.'
-    });
-  }
-
-  try {
-    // Hashear la contraseña
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-    // Simular guardado en base de datos
-    console.log('Carnet:', carnet);
-    console.log('Password hasheada:', hashedPassword);
-
-    return res.json({ mensaje: 'Usuario registrado con éxito.' });
-  } catch (err) {
-    console.error('Error al hashear:', err);
-    return res.status(500).json({ error: 'Error interno del servidor' });
-  }
-});
-
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
