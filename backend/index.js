c5da0182a8de9cfab61d71ba7413f155b48c3675
mario-90 <105886653+mario-90@users.noreply.github.com>
2025-05-18 00:47:09 -0600

    registro

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
+const bcrypt = require('bcrypt');
 
 app.use(express.json());
 
@@ -12,9 +13,33 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-app.get('/registro', (req, res) => {
+app.post('/registro', async (req, res) => {
+  const { carnet, password } = req.body;
   console.log(req.body);
-  res.status(200).json({ mensaje: "Registro exitoso" });
+
+  // Validación de contraseña
+  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}$/;
+
+  if (!regex.test(password)) {
+    return res.status(400).json({
+      mensaje: 'La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula y un símbolo.'
+    });
+  }
+
+  try {
+    // Hashear la contraseña
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    // Simular guardado en base de datos
+    console.log('Carnet:', carnet);
+    console.log('Password hasheada:', hashedPassword);
+
+    return res.json({ mensaje: 'Usuario registrado con éxito.' });
+  } catch (err) {
+    console.error('Error al hashear:', err);
+    return res.status(500).json({ error: 'Error interno del servidor' });
+  }
 });
 
 app.listen(PORT, () => {
